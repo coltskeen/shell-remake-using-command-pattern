17ab617bbf770b825226fb1b39ec05bf24b288d7
Colt Skeen <colt.skeen@gesa.com>
2025-02-21 16:38:06 -0800

    feat: Create REPL my first attempt

--- src/main.cs
+++ src/main.cs
@@ -1,9 +1,15 @@
 using System.Net;
 using System.Net.Sockets;
 
-Console.Write("$ ");
+// Create a random number for REPL loops
+Random rnd = new();
+int loops = rnd.Next(1, 10);
 
-// Wait for user input
-var command = Console.ReadLine();
-Console.WriteLine($"{command}: command not found");
-
+// Allow execution of command a random number of times
+for (int i = 0; i < loops; i++)
+{
+    Console.Write("$ ");
+    // Wait for user input
+    var command = Console.ReadLine();
+    Console.WriteLine($"{command}: command not found");
+}
